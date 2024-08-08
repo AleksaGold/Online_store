8f176a8070c1758e5c5c8f6c5fc04f9c12ea0b10
Alexandra Goldobina <goldobina.s@gmail.com>
2024-08-08 16:39:40 +0500

    Задача 4

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'main/home.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
