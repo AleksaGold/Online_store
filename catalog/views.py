076a708a7c5b95e0837c1003cf4dd531bb325670
Alexandra Goldobina <goldobina.s@gmail.com>
2024-08-23 19:50:01 +0500

    Создан новый контроллер и шаблон, которые отвечают за отображение отдельной страницы с товаром

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -7,6 +7,12 @@ def products_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "main/home.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "main/product_detail.html", context)
 
 
 def contacts(request):
